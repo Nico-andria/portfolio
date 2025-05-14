6d2e7b4df36d2bab2144512fe053c8248eb54168
unknown <iangaly.nirina@gmail.com>
2025-05-14 17:00:00 +0300

    first commit of my portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Computer technician", "Trader"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+var emailtyped = new Typed(".email-typed", {
+  strings: ["iangaly.nirina@gmail.com", "nicoandria.pro@gmail.com"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
